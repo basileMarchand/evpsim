b9d90a52e0d9ab537dcc20aef8f3f3f2e882f8aa
Basile Marchand <basile.marchand@mat.mines-paristech.fr>
2023-07-27 16:40:49 +0200

    update example

--- simul_example.py
+++ simul_example.py
@@ -5,7 +5,7 @@ from scipy.integrate import solve_ivp
 from mecaexp.behavior.criterion import MisesCriterion
 from mecaexp.behavior.flow import NortonFlow
 from mecaexp.behavior.isotropic_hardening import LinearIsotropicHardening
-from mecaexp.behavior.kinematic_hardening import LinearKinematicHardening
+from mecaexp.behavior.kinematic_hardening import LinearKinematicHardening, NonLinearKinematicHardening
 from mecaexp.behavior.potential import PotentialEVP
 from mecaexp.behavior.genevp import GeneralizedElastoViscoPlastic
 from mecaexp.tools.elasticity_helper import isotropic_elasticity
@@ -13,18 +13,19 @@ from mecaexp.tools.elasticity_helper import isotropic_elasticity
 from mecaexp.simulator import SimuLoad, MaterialSimulator, MaterialWrapper
 
 criterion = MisesCriterion()
-flow = NortonFlow(100., 10.)
+flow = NortonFlow(100., 1.)
 iso = LinearIsotropicHardening(300.)
-kin1 = LinearKinematicHardening(1500.)
-pot = PotentialEVP(300., criterion, flow, iso, [kin1])
+kin1 = NonLinearKinematicHardening(60000., 400.)
+pot = PotentialEVP(100., criterion, flow, iso, [kin1])
 elas = isotropic_elasticity(110000., 0.3)
 beha = GeneralizedElastoViscoPlastic(elas, [pot])
 
 load = SimuLoad()
-load.addComponent('eto11', (0., 0.5, 1.), (0., 0.05, 0.), repeat=4)
+load.addComponent('eto11', (0., 0.5, 1., 1.5, 2.),
+                  (0., 0.01, 0., -0.01, 0.), repeat=4)
 
 simu = MaterialSimulator()
-simu.setDTime(1./40.)
+simu.setDTime(1./100.)
 simu.setMaterial(beha)
 simu.setLoad(load)
 simu.outputCycles((0, 1, 2, 3))
