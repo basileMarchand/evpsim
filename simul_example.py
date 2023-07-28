8dad3bbc8b53344399928cead48c55bd3201eb69
Basile Marchand <basile.marchand@mat.mines-paristech.fr>
2023-07-28 17:08:09 +0200

    update example

--- simul_example.py
+++ simul_example.py
@@ -15,23 +15,33 @@ from mecaexp.simulator import SimuLoad, MaterialSimulator, MaterialWrapper
 criterion = MisesCriterion()
 flow = NortonFlow(100., 1.)
 iso = LinearIsotropicHardening(300.)
-kin1 = NonLinearKinematicHardening(60000., 400.)
-pot = PotentialEVP(100., criterion, flow, iso, [kin1])
+kin1 = NonLinearKinematicHardening(60000., 400., 0)
+kin2 = NonLinearKinematicHardening(120000., 400., 1)
+
+pot = PotentialEVP(100., criterion, flow, iso, [kin1, kin2], name="ep")
+
+criterion2 = MisesCriterion()
+flow2 = NortonFlow(1000., 10.)
+iso2 = LinearIsotropicHardening(0.)
+pot2 = PotentialEVP(0., criterion2, flow2, iso2, [], name="ev")
+
 elas = isotropic_elasticity(110000., 0.3)
-beha = GeneralizedElastoViscoPlastic(elas, [pot])
+beha = GeneralizedElastoViscoPlastic(elas, [pot, pot2])
 
 load = SimuLoad()
-load.addComponent('eto11', (0., 0.5, 1., 1.5, 2.),
-                  (0., 0.01, 0., -0.01, 0.), repeat=4)
 
+load.addComponent('eto11', (0., 0.25, 0.5, 0.75, 1.),
+                  (0., 0.01, 0., -0.01, 0.), repeat=1)
+load.addComponent('eto11', (0., 0.5, 1., 1.5, 2.),
+                  (0., 0.015, 0., -0.015, 0.), repeat=1)
 simu = MaterialSimulator()
-simu.setDTime(1./100.)
+simu.setDTime(1./99.)
 simu.setMaterial(beha)
 simu.setLoad(load)
-simu.outputCycles((0, 1, 2, 3))
+simu.outputCycles([x for x in range(100)])
 _ = simu.updateOutputShapes()
 
-simu.compute()
+simu.compute(method="lm", options={"xtol": 1.e-2})
 
 plt.plot(simu._strain_history[:, 0, 0], simu._stress_history[:, 0, 0])
 plt.show()
